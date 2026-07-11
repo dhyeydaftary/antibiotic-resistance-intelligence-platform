a7c89e5f26abef617530ef7945297b1b74a97bbf
Dhyey <dhyey104daftary@gmail.com>
2026-07-11 13:47:01 +0530

    Add signup route with password hashing and JWT token

--- gateway/index.js
+++ gateway/index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 // Simple health check route
 app.get('/', (req, res) => {
